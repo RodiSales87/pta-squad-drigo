d2421e79f222058ee60000e8a35ed1ef2e5c8a0a
ThalesADG <thales.afonso@ufpe.br>
2024-12-11 15:31:42 -0300

    original page

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,44 +1,20 @@
 import Image from "next/image";
 
 import { LogoCITi } from "../assets";
-import Details from "@/app/GameDetails/gameDetails";
-import Cards from "@/components/cards/cards";
 
 export default function Home() {
   return (
-    // <div className="flex flex-1 flex-col h-full justify-around items-center bg-black">
-    //   <div>
-    //     <Image src={LogoCITi} alt="Logo citi" />
-    //   </div>
-    //   <div className="flex flex-col justify-center items-center">
-    //     <h1 className="text-white text-4xl font-bold">NextJS Boilerplate</h1>
-    //     <p className="text-white text-xl">
-    //       Made with <strong>&lt; &#x0002F; &gt;</strong> and{" "}
-    //       <strong>&hearts;</strong> by CITi
-    //     </p>
-    //   </div>
-    // </div>
-    <div>
-      <Details
-        id="Player4"
-        game="GameName"
-        date="2025-07-06"
-        hour="10:00"
-        contactPlatform="Platform Name"
-        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse eu rutrum mauris, quis ullamcorper urna. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse eu rutrum mauris, quis ullamcorper urna. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse eu rutrum mauris, quis ullamcorper urna. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse eu rutrum mauris, quis ullamcorper urna."
-        link="https://www.youtube.com"
-        participants={["Player1", "Player2", "Player3"]}
-        numParticipants={3}
-        maxParticipants={4}
-      />
-      <Cards
-        game="GameName"
-        date="2025-07-06"
-        hour="10:00"
-        contactPlatform="Platform Name"
-        numParticipants={3}
-        maxParticipants={4}
-      />
+    <div className="flex flex-1 flex-col h-full justify-around items-center bg-black">
+      <div>
+        <Image src={LogoCITi} alt="Logo citi" />
+      </div>
+      <div className="flex flex-col justify-center items-center">
+        <h1 className="text-white text-4xl font-bold">NextJS Boilerplate</h1>
+        <p className="text-white text-xl">
+          Made with <strong>&lt; &#x0002F; &gt;</strong> and{" "}
+          <strong>&hearts;</strong> by CITi
+        </p>
+      </div>
     </div>
   );
 }
