c98aa88b8344672a99f94e8d71cbecf2d48decd5
ThalesADG <thales.afonso@ufpe.br>
2024-12-12 02:07:40 -0300

    profile initialized

---
+++ client/src/app/Profile/profile.tsx
@@ -0,0 +1,95 @@
+"use client";
+import React, { useState } from "react";
+import Cards from "@/components/cards/cards";
+import { Barlow } from "next/font/google";
+import { Button } from "@/components/ui/button";
+
+const barlow400 = Barlow({
+  subsets: ["latin"],
+  weight: "400",
+});
+
+const barlow500 = Barlow({
+  subsets: ["latin"],
+  weight: "500",
+});
+
+const barlow700 = Barlow({
+  subsets: ["latin"],
+  weight: "700",
+});
+
+export default function Profile() {
+  const [username, setUsername] = useState(""); // Estado para o campo de texto
+  const [activeTab, setActiveTab] = useState("partidas"); // Estado para abas
+  return (
+    <div className="bg-[#F5F5F5] ml-[280px] mt-[64px] flex flex-col">
+      <div className="ml-[40px]">
+        <div>
+          <div className={`${barlow400.className} text-[32px] text-[#000000]`}>
+            Qual o username?
+          </div>
+          <div className="flex flex-row items-center gap-[30px]">
+            <input
+              type="text"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              placeholder="Digite seu username"
+              className={`border border-[#D9D9D9] rounded-[4px] px-[10px] py-[6px] text-[#292929] text-[16px] w-[320px] h-[52px] ${barlow400.className}`}
+            />
+            <Button
+              className={`bg-[#58CBFB] border-[#D9D9D9] text-[#F5F5F5] text-[16px] ${barlow700.className} w-[100px] h-[40px] rounded-[15px]`}
+              style={{
+                boxShadow: "0px 4px 4px 0px rgba(0, 0, 0, 0.25)",
+              }}
+            >
+              Buscar
+            </Button>
+          </div>
+        </div>
+        <div>
+          <div className="flex flex-row gap-[50px] border-b border-[#DEDEDE] mb-[20px]">
+            <div
+              onClick={() => setActiveTab("partidas")}
+              className={`cursor-pointer pb-[8px] ml-[51px] ${
+                activeTab === "partidas"
+                  ? "border-b-[2px] border-[#6800E4] text-[#6800E4]"
+                  : "text-[#454545]"
+              } text-[16px] ${barlow400.className}`}
+            >
+              Partidas
+            </div>
+            <div
+              onClick={() => setActiveTab("historico")}
+              className={`cursor-pointer pb-[8px] ${
+                activeTab === "historico"
+                  ? "border-b-[2px] border-[#6800E4] text-[#6800E4]"
+                  : "text-[#454545]"
+              } text-[16px] ${barlow400.className}`}
+            >
+              Histórico
+            </div>
+          </div>
+          <div>
+            {activeTab === "partidas" ? (
+              <div className="flex flex-col gap-[10px]">
+                <Cards
+                  game="Minecraft"
+                  date="2024-12-12"
+                  hour="20:00"
+                  contactPlatform="Discord"
+                  numParticipants={4}
+                  maxParticipants={5}
+                />
+              </div>
+            ) : (
+              <div className="text-[#454545]">
+                Aqui está o histórico das suas partidas.
+              </div>
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,20 +1,37 @@
 import Image from "next/image";
 
 import { LogoCITi } from "../assets";
+import Profile from "@/app/Profile/profile";
+import Details from "@/app/GameDetails/gameDetails";
 
 export default function Home() {
   return (
-    <div className="flex flex-1 flex-col h-full justify-around items-center bg-black">
-      <div>
-        <Image src={LogoCITi} alt="Logo citi" />
-      </div>
-      <div className="flex flex-col justify-center items-center">
-        <h1 className="text-white text-4xl font-bold">NextJS Boilerplate</h1>
-        <p className="text-white text-xl">
-          Made with <strong>&lt; &#x0002F; &gt;</strong> and{" "}
-          <strong>&hearts;</strong> by CITi
-        </p>
-      </div>
-    </div>
+    // <div className="flex flex-1 flex-col h-full justify-around items-center bg-black">
+    //   <div>
+    //     <Image src={LogoCITi} alt="Logo citi" />
+    //   </div>
+    //   <div className="flex flex-col justify-center items-center">
+    //     <h1 className="text-white text-4xl font-bold">NextJS Boilerplate</h1>
+    //     <p className="text-white text-xl">
+    //       Made with <strong>&lt; &#x0002F; &gt;</strong> and{" "}
+    //       <strong>&hearts;</strong> by CITi
+    //     </p>
+    //   </div>
+    // </div>
+
+    <Profile />
+
+    // <Details
+    //   id="1"
+    //   game="Valorant"
+    //   date="2022-11-11"
+    //   hour="12:00"
+    //   contactPlatform="Discord"
+    //   description="Vamos jogar Valorant?"
+    //   link="https://discord.gg/valorant"
+    //   participants={["1", "2", "3"]}
+    //   numParticipants={3}
+    //   maxParticipants={5}
+    // />
   );
 }
